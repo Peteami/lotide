b9cf918d45a1c80e34d8122afabe3fdc6f7c03e5
Peteami <peteami@hotmail.com>
2020-08-19 17:10:12 +0000

    used linter on assertArrayEqual

--- assertArrayEqual.js
+++ assertArrayEqual.js
@@ -4,13 +4,12 @@ const eqArrays = function(array1, array2) {
   } else {
     for (let i = 0; i < array1.length; i++) {
       if (array1[i] !== array2[i]) {
-        // console.log(array1 + array2 + "false");
-        return false; 
-      }  
+        return false;
+      }
     }
     return true;
   }
-}
+};
 
 
 
@@ -18,15 +17,15 @@ const eqArrays = function(array1, array2) {
 const assertArrayEqual = function(array1, array2) {
   const no = String.fromCodePoint(0x1f631);
   const yes = String.fromCodePoint(0x1f44b);
-  let isEqual = eqArrays(array1, array2)
+  let isEqual = eqArrays(array1, array2);
   if (isEqual === true) {
     console.log(`${yes} ${yes} ${yes} Assertion Passed: ${array1} === ${array2}`);
   } else {
     console.log(`${no} ${no} ${no} Assertion Failed: ${array1} !==  ${array2}`);
-  }  
+  }
 };
 
 
 
-assertArrayEqual([1, 2, 3], [1, 5, 6]) // => false
-assertArrayEqual([1, 2, 3], [1, 2, 3]) // => true
+assertArrayEqual([1, 2, 3], [1, 5, 6]); // => false
+assertArrayEqual([1, 2, 3], [1, 2, 3]); // => true
