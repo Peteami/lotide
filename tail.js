66e8a1608dec2b6a7823bb4f8f816645237ca1d5
Peteami <peteami@hotmail.com>
2020-08-24 13:21:20 +0000

    2nd commit refactor tail

--- tail.js
+++ tail.js
@@ -1,13 +1,5 @@
-const assertEqual = function(actual, expected) {
-  const no = String.fromCodePoint(0x1f631);
-  const yes = String.fromCodePoint(0x1f44b);
-  if (actual === expected) {
-    console.log(`${yes} ${yes} ${yes} Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`${no} ${no} ${no} Assertion Failed: ${actual} !==  ${expected}`);
-  }
-    
-};
+const assertEqual = require('./assertEqual');
+
 
 const tail = function(arr) {
   let newarr = arr.shift();
