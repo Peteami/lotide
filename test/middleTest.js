246819ae7aff6f1b4109d9e819c0f4b937bcd752
Peteami <peteami@hotmail.com>
2020-08-24 18:22:16 +0000

    refactor tailTest and middleTest

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,5 +1,15 @@
+const assert = require('chai').assert;
 const middle = require('../middle');
 
 
-console.log(middle([2, 5, 9, 6, 4, 7]));
-console.log(middle([2,1,3]));
+describe("#middle", () => {
+
+  it("returns [9, 6] for [2, 5, 9, 6, 4, 7]", () => {
+    assert.deepEqual(middle([2, 5, 9, 6, 4, 7]), [9, 6]);
+  })
+
+  it("returns 1 for [2 ,1 ,3]", () => {
+    assert.deepEqual(middle([2 ,1 ,3]), 1);
+  })
+
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,8 +1,19 @@
+const assert = require('chai').assert;
 const assertEqual = require('../assertEqual');
 const tail = require('../tail');
 
 
+describe("#tail", () => {
 
+  it("returns [9, 6] for [2, 5, 9, 6, 4, 7]", () => {
+    assert.deepEqual(tail([2, 5, 9, 6, 4, 7]), [9, 6]);
+  })
+
+  it("returns 1 for [2 ,1 ,3]", () => {
+    assert.deepEqual(tail([2 ,1 ,3]), 1);
+  })
+
+});
 
 // Test Case: Check the original array
 const words = ["Yo Yo", "Lighthouse", "Labs"];
