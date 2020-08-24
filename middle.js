11cabf428f8c30bd95a80f57f457dd935884e2ed
Peteami <peteami@hotmail.com>
2020-08-24 17:22:26 +0000

    refactor middle.js and add in test folder with middleTest

--- middle.js
+++ middle.js
@@ -1,27 +1,4 @@
-const assertArrayEqual = function(array1, array2) {
-  const no = String.fromCodePoint(0x1f631);
-  const yes = String.fromCodePoint(0x1f44b);
-  let isEqual = eqArrays(array1, array2);
-  if (isEqual === true) {
-    console.log(`${yes} ${yes} ${yes} Assertion Passed: ${array1} === ${array2}`);
-  } else {
-    console.log(`${no} ${no} ${no} Assertion Failed: ${array1} !==  ${array2}`);
-  }
-};
-
-
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  } else {
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] !== array2[i]) {
-        return false; 
-      }  
-    }
-    return true;
-  }
-}
+const eqArrays = require('./eqArrays');
 
 
 const middle = function(arr) {
@@ -37,5 +14,6 @@ const middle = function(arr) {
   return [arr[result1], arr[result2]];
 };
 
-console.log(middle([2, 5, 9, 6, 4, 7]));
-console.log(middle([2,1,3]));
+
+
+module.exports = middle;
---
+++ test/middleTest.js
@@ -0,0 +1,5 @@
+const middle = require('../middle');
+
+
+console.log(middle([2, 5, 9, 6, 4, 7]));
+console.log(middle([2,1,3]));
