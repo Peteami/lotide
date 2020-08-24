1434f3d03f2341740d00bb3221cdf2b0bcb81a73
Peteami <peteami@hotmail.com>
2020-08-24 12:37:31 +0000

    refactor of assertEqual and head

--- assertEqual.js
+++ assertEqual.js
@@ -2,17 +2,12 @@ const assertEqual = function(actual, expected) {
   const no = String.fromCodePoint(0x1f631);
   const yes = String.fromCodePoint(0x1f44b);
   if (actual === expected) {
-    console.log(yes + yes + yes + " Assertion Passed: " + actual + " === " + expected);
+    console.log(`${yes} ${yes} ${yes} Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log(no + no + no + " Assertion Failed: " + actual + " !== " + expected);
+    console.log(`${no} ${no} ${no} Assertion Failed: ${actual} !==  ${expected}`);
   }
     
 };
 
 
-
-// TEST CODE
-
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-
+module.exports = assertEqual;
--- head.js
+++ head.js
@@ -1,19 +1,9 @@
-const assertEqual = function(actual, expected) {
-  const no = String.fromCodePoint(0x1f631);
-  const yes = String.fromCodePoint(0x1f44b);
-  if (actual === expected) {
-    console.log(`${yes} ${yes} ${yes} Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`${no} ${no} ${no} Assertion Failed: ${actual} !==  ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 
 const head = function(arr) {
-  let ind = arr[0];
-  return ind;
+  return arr[0];
 }
  
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
