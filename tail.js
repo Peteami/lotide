22e76a3ed9b94ceade3a90f37a4f86f241c998ba
Peteami <peteami@hotmail.com>
2020-08-24 13:10:04 +0000

    initial commit tail.js refactored with updated test

--- tail.js
+++ tail.js
@@ -15,7 +15,4 @@ const tail = function(arr) {
 };
 
 
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 2); // original array should still have 3 elements!
+module.exports = tail;
