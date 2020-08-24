fcd651d0e48a5487ea7075ca940d5e9d806bd667
Peteami <peteami@hotmail.com>
2020-08-24 13:28:05 +0000

    refactor eqArrays and add test/eqArraysTest

--- eqArrays.js
+++ eqArrays.js
@@ -1,24 +1,5 @@
-const assertEqual = function(actual, expected) {
-  const no = String.fromCodePoint(0x1f631);
-  const yes = String.fromCodePoint(0x1f44b);
-  if (actual === expected) {
-    console.log(`${yes} ${yes} ${yes} Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`${no} ${no} ${no} Assertion Failed: ${actual} !==  ${expected}`);
-  }
-    
-};
+const assertEqual = require('./assertEqual');
 
-
-
-// const eqArrays = function(array1, array2) {
-//   if (JSON.stringify(array1) === JSON.stringify(array2)) {
-//     return true;
-//   } else {
-//     return false;
-//   }
-
-// }
 
 const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) {
@@ -26,7 +7,6 @@ const eqArrays = function(array1, array2) {
   } else {
     for (let i = 0; i < array1.length; i++) {
       if (array1[i] !== array2[i]) {
-        // console.log(array1 + array2 + "false");
         return false; 
       }  
     }
@@ -35,13 +15,4 @@ const eqArrays = function(array1, array2) {
 }
 
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true) // => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false) // => false
-
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true) // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false) // => false
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 77]), false) // => false
-
-
-assertEqual(eqArrays(["1", "2"], ["1", "2", "3"]), false) 
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2"]), false) 
+module.exports = eqArrays;
