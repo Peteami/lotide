714633c0221d2d0ea415f1f7c9fec2f690c7915d
Peteami <peteami@hotmail.com>
2020-08-24 19:39:47 +0000

    refactor other functions in test folder

--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -1,6 +1,21 @@
+const assert = require('chai').assert;
 const eqArrays = require('../eqArrays');
 const assertArrayEqual = require('../assertArrayEqual');
 
 
-assertArrayEqual([1, 2, 3], [1, 5, 6]); // => false
-assertArrayEqual([1, 2, 3], [1, 2, 3]); // => true
+// assertArrayEqual([1, 2, 3], [1, 5, 6]); // => false
+// assertArrayEqual([1, 2, 3], [1, 2, 3]); // => true
+
+
+
+describe("assertArrayEqual", () => {
+
+  it("returns false for ([1, 2, 3]), [1, 5, 6])", () => {
+    assert.strictEqual(assertArrayEqual([1, 2, 3]), [1, 5, 6]);
+  })
+
+  it("returns true for ([1, 2, 3], [1, 2, 3])", () => {
+    assert.strictEqual(assertArrayEqual([1, 2, 3]), [1, 2, 3]);
+  })
+
+});
